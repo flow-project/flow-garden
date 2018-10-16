12e045ff373d1217d67cb149e70741177f173266
maheshmurag <maheshmurag@berkeley.edu>
2018-10-16 10:26:38 -0700

    Added submission route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var benchmarkRouter = require('./routes/benchmark');
+var submissionRouter = require('./routes/submission');
 
 var app = express();
 
@@ -16,13 +17,14 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/benchmark', benchmarkRouter);
+app.use('/submission', submissionRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/submission.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+
+// const http = require('http');
+const fs = require('fs');
+const unzip = require('unzip');
+var requestLib = require('request');
+const bodyParser = require("body-parser");
+
+router.get('/', function(request, response) {
+	response.send('Hello World');
+});
+
+router.post('/forms', function(request, response) {
+	var answers = request.body;
+	console.log(answers);
+	var name = answers["Name"];
+	var email = answers["Email"];
+	var fileURL = "https://drive.google.com/uc?export=download&id=" + answers["Submission"];
+	var benchmark = answers["Benchmark"];
+
+	console.log("Attempting download from: " + fileURL);
+	requestLib(fileURL).on('response', function(fileResponse) {
+
+		var fileName = "submission_" + Date.now();
+		var file = fs.createWriteStream(fileName + ".zip");
+		fileResponse.pipe(file);
+		
+		file.on('error', function (err) {
+        	console.log("ERROR!: " + err);
+    	});
+
+		file.on('close', function() {
+			console.log("Upload successful! Unzipping.")
+			fs.createReadStream(fileName + ".zip")
+				.pipe(unzip.Extract({ path: 'submissions/' + benchmark + '/' + fileName }))
+				.on('close', function(e) {
+					console.log("Finished unzipping! (" + e + ")")
+					processFile(fileName)
+				})
+		});
+	});
+	response.sendStatus(200)
+})
+
+// move following to /utils ?
+function processFile(folderName) {
+	console.log("Retrieving reward for " + folderName)
+	// call get_reward function with extracted config and controller function
+	// get score back from reward function
+	// commit new .yml file with to github branch (or whichever schema is agreed upon)
+		// .yml file will contain name, email, instituion, and score
+
+	// Step 1. Read the input yaml config file that's placed in the folderName directory. Extract the environment file name and environment class name
+			// For now, it might be worth it to just encode that as part of the Google Form for simplicity and avoidance of this extra step. In addition,
+			// it might be worth it to simply pass these in as input params into the test_bm script ("python test_bm --benchmark='abc' --env-name=-test-'")
+
+	// Step 2. Call test_bm(folderName). Jonathan's script should access the correct folder/solution_config.yaml file, or if we go with the alternate option,
+			// should get the necessary input as command line args. His script should then output either an error (which I should pipe to a log file) 
+			// or a successful reward value.
+
+	// Step 3. If everything has succeeded thus far, I need to read the benchmarks/benchmark_name.yml file as an object, update the object with the newest score,
+			// email, link to explanation, name, description, and then write back to the file. This will be used by Kevin's stuff.
+
+}
+
+module.exports = router;
