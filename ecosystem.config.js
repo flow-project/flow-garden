8ae5d044b1aba17b44e540fcfa55dc76a3c6fbc6
Mahesh (from EC2) <maheshmurag@berkeley.edu>
2018-10-26 23:23:46 +0000

    Update pm2 settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     autorestart: true,
     watch: true,
-    ignore_watch: ['node_modules', 'data'],
+    ignore_watch: ['.git', 'node_modules', 'data'],
     max_memory_restart: '2G',
     env: {
       NODE_ENV: 'development'
