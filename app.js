91090be59ac2f6a5eedcc0352452a9c4b1335b7d
Kevin Chien <kevinchien17@gmail.com>
2018-11-02 18:28:51 -0700

    removed compression, cookie-parser

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var compression = require('compression');
 
 var indexRouter = require('./routes/index');
 var benchmarkRouter = require('./routes/benchmark');
@@ -14,7 +13,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(compression);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
