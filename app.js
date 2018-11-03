8a6d43cf071f4a4141adc748bf594e8a5531acf5
Kevin Chien <kevinchien17@gmail.com>
2018-11-02 18:13:00 -0700

    added compression, removed cookies-parser, added prod mode command

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
 
 var indexRouter = require('./routes/index');
 var benchmarkRouter = require('./routes/benchmark');
@@ -14,14 +14,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(compression);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/benchmark', benchmarkRouter);
+app.use('/bm', benchmarkRouter);
 app.use('/submission', submissionRouter);
 
 // catch 404 and forward to error handler
